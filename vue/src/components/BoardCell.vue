37d52405bf963f14230a0e4c8a7d2c9b1c32552f
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-03-26 02:46:55 +0300

    add file

---
+++ vue/src/components/BoardCell.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+      class="game-board__cell"
+      :style="{
+      backgroundColor: cell.color,
+      border: isSelected ? '2px solid #000' : 'none'
+    }"
+      @click="handleClick"
+  ></div>
+</template>
+<script>
+export default {
+  props: {
+    rowIndex: Number,
+    colIndex: Number,
+    cell: Object,
+    isSelected: Boolean,
+    handleCellClick: Function,
+  },
+  methods: {
+    handleClick(e) {
+      this.handleCellClick(this.rowIndex, this.colIndex, e);
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+@import '../styles/_board-cell.less';
+</style>
---
+++ vue/src/components/BoardRow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="game-board__row">
+    <BoardCell
+        v-for="(cell, colIndex) in row"
+        :key="colIndex"
+        :rowIndex="rowIndex"
+        :colIndex="colIndex"
+        :cell="cell"
+        :isSelected="isSelected(rowIndex, colIndex)"
+        :handleCellClick="handleCellClick"
+    />
+  </div>
+</template>
+<script>
+import BoardCell from './BoardCell.vue';
+export default {
+  components: {
+    BoardCell
+  },
+  props: {
+    row: Array,
+    rowIndex: Number,
+    isSelected: Function,
+    handleCellClick: Function,
+  }
+};
+</script>
+<style lang="less" scoped>
+@import '../styles/_board-row.less';
+</style>
